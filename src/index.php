8ecc0400e2e013db42df9284d64a0ec324cb237a
Lucy <74199226+Lucyginger@users.noreply.github.com>
2021-10-11 21:57:19 +1000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,4 +1,8 @@
 <?php 
+include_once 'header.php';
 echo "I am a PHP page running on four containers: NginX + PHP-PFM + MariaDB + Redis!";
-phpinfo();
 ?>
+
+    <section class='container'>
+        <h1> Some random things </h1>
+    </section>
